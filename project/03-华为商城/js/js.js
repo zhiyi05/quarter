56786c1c7067bf88be664f31e059935fcef08c74
zhiyi <787990099@qq.com>
2023-01-11 21:50:15 +0800

    Merge remote-tracking branch 'origin/main'

--- project/03-华为商城/js/js.js
+++ project/03-华为商城/js/js.js
@@ -3,18 +3,60 @@ var serverURL = "https://res.vmallres.com/pimages"
 var ulEl = document.querySelector(".products")
 var sectionEl = document.querySelector(".section")
 
-for (var ii = 0; ii < resultList.length; ii++) {
-    var resultEl = resultList[ii]
-    var itemEl = document.createElement("li")
-    itemEl.className = "item"
-    ulEl.append(itemEl)
 
-    var serviceString = ""
-    for (var label of resultEl.promoLabels) {
-        serviceString += `<div class="tip"> ${label} </div>`
+var filterEls = []
+// button.listener
+var btnEls = sectionEl.querySelectorAll(".operation")
+for (var i = 1; i < btnEls.length; i++) {
+    btnEls[i].onclick = function () {
+
+        this.classList.toggle("active")
+        var text = this.textContent.trim()
+        if (this.classList.contains("active") && !filterEls.includes(text)) {
+            filterEls.push(text)
+        } else {
+            var index = filterEls.findIndex(function (item) {
+                return item === text
+            })
+            filterEls.splice(index, 1)
+        }
+        // showFilter()
+        fliterList()
     }
+}
 
-    itemEl.innerHTML = `
+var filterList = resultList
+function fliterList() {
+// content of filter
+    filterList = resultList.filter(function (item) {
+        var flag = true
+        for (var filterEl of filterEls) {
+            if (!item.services.includes(filterEl)) {
+                flag = false
+                break
+            }
+        }
+        return flag
+    })
+    show()
+}
+
+
+show()
+function show() {
+    // show
+    ulEl.innerHTML = ""
+    for (var ii = 0; ii < filterList.length; ii++) {
+        var resultEl = filterList[ii]
+        var itemEl = document.createElement("li")
+        itemEl.className = "item"
+        ulEl.append(itemEl)
+
+        var serviceString = ""
+        for (var label of resultEl.promoLabels) {
+            serviceString += `<div class="tip"> ${label} </div>`
+        }
+        itemEl.innerHTML = `
             <a href="#">
                 <div class="top">
                     <img src=${serverURL}${resultEl.photoPath}428_428_${resultEl.photoName} alt="" class="picture">
@@ -36,25 +78,8 @@ for (var ii = 0; ii < resultList.length; ii++) {
                 </div>
             </a>
     `
-}
-
-
-var filterEl = []
-// button.listener
-var btnEls = sectionEl.querySelectorAll(".operation")
-for (var i = 1; i < btnEls.length; i++) {
-    btnEls[i].onclick = function () {
-
-        this.classList.toggle("active")
-        var text = this.textContent.trim()
-        if (this.classList.contains("active") && !filterEl.includes(text)) {
-            filterEl.push(text)
-        } else {
-            var index = filterEl.findIndex(function (item) {
-                return item === text
-            })
-            filterEl.splice(index, 1)
-        }
 
     }
 }
+
+
